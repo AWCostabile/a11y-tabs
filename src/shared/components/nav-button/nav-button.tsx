732cc60922e446c0512646433bbbdd672dfdafe3
Anthony Costabile <anthony@tallyit.com.au>
2021-02-23 07:36:51 +1100

    refactor: use ActiveKey context to derive control key state

--- src/shared/components/nav-button/nav-button.tsx
+++ src/shared/components/nav-button/nav-button.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button, IButtonProps } from 'shared/components/button';
+import { TrackedKeyType } from 'shared/constants/app';
+import { ClickEvent } from 'shared/types/general';
+import { ActiveKeysContext } from '../active-keys';
+import { IActiveKeysContext } from '../active-keys/active-keys-types';
 
 interface INavButtonProps
   extends Omit<IButtonProps, 'onClick' | 'onMiddleClick'> {
@@ -13,12 +17,23 @@ export const NavButton: React.FC<INavButtonProps> = ({
   ...props
 }) => {
   const history = useHistory();
+  const { isActive }: IActiveKeysContext<TrackedKeyType> = useContext(
+    ActiveKeysContext,
+  );
+
+  const onCurrentWindow = () => history.push(to);
+  const onNewWindow = (event: ClickEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.currentTarget?.blur();
+
+    window.open(to);
+  };
 
   return (
     <Button
       {...props}
-      onClick={() => history.push(to)}
-      onMiddleClick={() => window.open(to)}
+      onClick={isActive('Control') ? onNewWindow : onCurrentWindow}
+      onMiddleClick={onNewWindow}
     >
       {children}
     </Button>
